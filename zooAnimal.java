aa15fde5ca2f5249781faff262d5297453744e57
bean411 <118022383+bean411@users.noreply.github.com>
2022-11-11 15:36:56 -0600

    Update zooAnimal.java

--- zooAnimal.java
+++ zooAnimal.java
@@ -4,6 +4,20 @@ private String animalName;
 private int animalNum;
 private String enclosure;
 //food
+public zooAnimal(){
+  animalName = "No name inputted.";
+  animalNum = 0;
+  enclosure = "No enclosure specified.";
+}
+public zooAnimal(String name, int num, String enc){
+  animalName = name;
+  animalNum = num;
+  enclosure = enc;
+}
+  
+public void setAnimalName(String n){
+  animalName = n;
+}
 //get animal num
 //get enclosure
 //get foodName (maybe in zooFood)
